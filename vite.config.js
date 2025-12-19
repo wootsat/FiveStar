b27a34e8d0afe45106c2b0722cd42ed4466a1e9d
ZiiiMaschine <wootsat@gmail.com>
2025-12-19 10:46:53 -0600

    updated vite config file

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/fivestar/', // <--- THIS LINE IS CRITICAL. MUST MATCH YOUR REPO NAME.
 })
